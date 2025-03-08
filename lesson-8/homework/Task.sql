1a1b58be8ba4d076af3e7009923ced66bf37d1ae
abdulaziz2212 <tulaganovabdulaziz04@gmail.com>
2025-03-08 16:00:01 +0500

    Subqueries

---
+++ lesson-8/homework/Task.sql
@@ -0,0 +1,37 @@
+use lesson8;
+
+--Task1 Write an SQL statement that counts the consecutive values in the Status field.
+-- We were asked to solve all problems with subqueries, no table expressions and no other advanced topics which we didn't see so far can be used
+select * from Groupings
+select MIN(StepNumber) as MinStepNumber, Max(StepNumber) as MaxStepNumber, Status, Count(*) as ConsecutiveCount
+FROM(
+	select 
+		StepNumber,
+		Status,
+		StepNumber - ROW_NUMBER() OVER (PARTITION BY Status ORDER BY StepNumber) AS GroupID
+	from Groupings
+) as sub
+GROUP BY Status, GroupID
+Order BY MinStepNumber;
+
+--Task2 Find all the year-based intervals from 1975 up to current when the company did not hire employees.
+
+select *
+from
+(
+select
+	CASE 
+		WHEN interval < 2 or hireYear  < 1975 then NULL
+		WHEN interval >= 2  then CONCAT(CAST(HireYear + 1 as NVARCHAR), '-', CAST(nextYear - 1 as NVARCHAR))
+		WHEN nextYear is null then CONCAT(CAST(HireYear + 1 as NVARCHAR), '-', CAST((SELECT MAX(YEAR(hire_date)) FROM Employees_n) AS NVARCHAR))
+	END as Years
+FROM
+	(select 
+		distinct year(hire_date) as hireYear, 
+		LEAD(year(hire_date)) over(order by hire_date) as nextYear,
+		LEAD(year(hire_date)) over(order by hire_date) - year(hire_date) as interval
+	from Employees_n
+	) as sub
+	) as sub2
+where years is not null;
+ 
